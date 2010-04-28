053f1c437bf02fcd1ae456513dd9da40d177f4ec
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2010-04-28 10:47:26 +0200

    proper licensing information (LGPL-2.1). Hope I get it right this time.

--- doc/conf.py
+++ doc/conf.py
@@ -10,6 +10,23 @@
 #
 # All configuration values have a default value; values that are commented out
 # serve to show the default value.
+# copyright 2004-2010 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
+#
+# This file is part of rql.
+#
+# rql is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# rql is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with rql. If not, see <http://www.gnu.org/licenses/>.
 
 import sys, os
 
