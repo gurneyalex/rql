f07f5b55aba19cd7d0024d3f63e812c6e5694ecb
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2010-09-09 08:43:15 +0200

    consider subquery aliases in Select.clean_solutions

--- stmts.py
+++ stmts.py
@@ -626,13 +626,15 @@ class Select(Statement, nodes.EditableMixIn, ScopeNode):
             solutions = self.solutions
         # this may occurs with rql optimization, for instance on
         # 'Any X WHERE X eid 12' query
-        if not self.defined_vars:
+        if not (self.defined_vars or self.aliases):
             self.solutions = [{}]
         else:
             newsolutions = []
             for origsol in solutions:
                 asol = {}
                 for var in self.defined_vars:
+                    asol[var] = origsol[var]
+                for var in self.aliases:
                     asol[var] = origsol[var]
                 if not asol in newsolutions:
                     newsolutions.append(asol)
