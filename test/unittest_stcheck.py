738a60d18551d2d734f2c8ad850d68ad62767564
Julien Cristau <julien.cristau@logilab.fr>
2014-07-28 11:21:08 +0200

    [test] drop __future__.with_statement import
    We no longer support python 2.5.

--- test/unittest_stcheck.py
+++ test/unittest_stcheck.py
@@ -15,8 +15,6 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with rql. If not, see <http://www.gnu.org/licenses/>.
-from __future__ import with_statement
-
 from logilab.common.testlib import TestCase, unittest_main
 
 from rql import RQLHelper, BadRQLQuery, stmts, nodes
